d9fc7081d9a81c88c1781f3ea9ed1716b7e8ee57
Benjamin <benjiji77@hotmail.fr>
2021-02-05 17:51:03 +0100

    Update

---
+++ IDSTORE2/IDSTORE2/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using IDSTORE2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace IDSTORE2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+      public String Get()
+        {
+            return "ok";
+        }
+
+      
+    }
+}
+
