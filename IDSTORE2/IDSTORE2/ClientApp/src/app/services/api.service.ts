bf183fd9ddfc1a1af1972592a9773e05d80551e9
Benjamin <benjiji77@hotmail.fr>
2021-03-12 14:28:09 +0100

    Change api URL in front api.service.ts

--- IDSTORE2/IDSTORE2/ClientApp/src/app/services/api.service.ts
+++ IDSTORE2/IDSTORE2/ClientApp/src/app/services/api.service.ts
@@ -21,25 +21,25 @@ export class ApiService {
     this.title.next(data);
     this.title.subscribe(data => this.test = data);
     console.log(this.test);
-    return this.http.get<Fichier[]>('https://localhost:44373/file/getByClasse/' + this.test);
+    return this.http.get<Fichier[]>('https://localhost:5001/file/getByClasse/' + this.test);
   }
 
   public getFile(): Observable<Fichier[]> {
-    return this.http.get<Fichier[]>('https://localhost:44373/file/')
+    return this.http.get<Fichier[]>('https://localhost:5001/file/')
   }
 
   public getFileByClasse(classe : string): Observable<Fichier[]> {
     this.title.subscribe(data => this.test = data);
     classe = this.test;
 
-    return this.http.get<Fichier[]>('https://localhost:44373/file/getByClasse/' + classe);
+    return this.http.get<Fichier[]>('https://localhost:5001/file/getByClasse/' + classe);
 
   }
 
 
   public downloadFile(name: string): Observable<Blob>  {
-    console.log('https://localhost:44373/file/dl/' + name);
-    return this.http.get('https://localhost:44373/file/dl/' + name, { responseType: 'blob' });
+    console.log('https://localhost:5001/file/dl/' + name);
+    return this.http.get('https://localhost:5001/file/dl/' + name, { responseType: 'blob' });
   }
 
 
