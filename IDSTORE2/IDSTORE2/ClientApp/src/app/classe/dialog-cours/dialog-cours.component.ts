a096a0c33db98a641e159833afd65f4f186d151e
dorianBellone <51541369+dorianBellone@users.noreply.github.com>
2021-06-18 16:30:37 +0200

    add first selector

--- IDSTORE2/IDSTORE2/ClientApp/src/app/classe/dialog-cours/dialog-cours.component.ts
+++ IDSTORE2/IDSTORE2/ClientApp/src/app/classe/dialog-cours/dialog-cours.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog-cours',
   templateUrl: './dialog-cours.component.html',
   styleUrls: ['./dialog-cours.component.css']
 })
+
+interface Classe {
+  nom: string;
+}
+
+
 export class DialogCoursComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  
+
+  classes: Classe[] = [
+    { nom: 'B1' },
+    { nom: 'B2' },
+    { nom: 'B3' },
+    { nom: 'M1' },
+    { nom: 'M2' }
+  ];
 
 }
