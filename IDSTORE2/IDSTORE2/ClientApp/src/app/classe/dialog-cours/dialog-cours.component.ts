71750040cdde0355ba95259c101dc5af136fd400
dorianBellone <51541369+dorianBellone@users.noreply.github.com>
2021-06-18 17:30:01 +0200

    mm

--- IDSTORE2/IDSTORE2/ClientApp/src/app/classe/dialog-cours/dialog-cours.component.ts
+++ IDSTORE2/IDSTORE2/ClientApp/src/app/classe/dialog-cours/dialog-cours.component.ts
@@ -6,6 +6,9 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 interface Classe {
   nom: string;
 }
+interface Matiere {
+  matiere: string;
+}
 
 @Component({
   selector: 'app-dialog-cours',
@@ -17,13 +20,20 @@ interface Classe {
 export class DialogCoursComponent implements OnInit {
   @Output() public onUploadFinished = new EventEmitter();
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private http: HttpClient) { }
-
+  constructor( private http: HttpClient) { }
+  selectedValue: string;
+  selectedValueM: string;
   public progress: number;
   public message: string;
+  public ok: string;
 
   ngOnInit() {
   }
+
+  matieresAll: Matiere[] = [{ matiere: 'Sciences Appliquées' }, { matiere: 'Algorithmique' }, { matiere: 'Méthodologie' }, { matiere: 'Electronique' }, { matiere: 'Architecture des systèmes d’informations' }, { matiere: 'Système d’exploitation' }, { matiere: 'Réseaux' }, { matiere: 'Outils Bureautiques' },
+  { matiere: 'Langages C' }, { matiere: 'PHP' }, { matiere: 'Javascript' }, { matiere: 'Langage pour le web' }, { matiere: 'Manipulation des données' }, { matiere: 'Anglais' }, { matiere: 'Expresion orale' }, { matiere: 'Expresion écrite' }, { matiere: 'Economie' }, { matiere: 'Droit' }, { matiere: 'Culture informatique' },
+  { matiere: 'IDlabs' }, { matiere: 'Projet individuel' }, { matiere: 'Stage' }];
+
 
   classes: Classe[] = [
     { nom: 'B1' },
@@ -52,7 +62,89 @@ export class DialogCoursComponent implements OnInit {
         }
       });
   }
-  
 
+  public check(value) :String {
+    switch (this.selectedValueM) {
+      case 'Sciences Appliquées': {
+        value = 'SC'
+        break;
+      }
+      case 'Algorithmique': {
+        value = 'ALGO';
+        break;
+      }
+      case 'Electronique': {
+        value = 'ELEC';
+        break;
+      }
+      case 'Méthodologie': {
+        value = 'METH';
+        break;
+      }
+      case 'Architecture des systèmes d\'informations': {
+        value = 'ARCHI';
+        break;
+      }
+      case 'Réseaux': {
+        value = 'RES';
+        break;
+      }
+      case 'Système d\'exploitation': {
+        value = 'SYS';
+        break;
+      }
+      case 'Outils Bureautiques': {
+        value = 'SOFT';
+        break;
+      }
+      case 'Langages C': {
+        value = 'CCPP';
+        break;
+      }
+      case 'Javascript': {
+        value = 'JSCRIPT';
+        break;
+      }
+      case 'Langage pour le web': {
+        value = 'W3C';
+        break;
+      }
+      case 'Anglais': {
+        value = 'ANG';
+        break;
+      }
+      case 'Expresion orale': {
+        value = 'ORAL';
+        break;
+      }
+      case 'Expresion écrite': {
+        value = 'EXPR';
+        break;
+      }
+      case 'Economie': {
+        value = 'ECO';
+        break;
+      }
+      case 'Droit': {
+        value = 'DRT';
+        break;
+      }
+      case 'Culture informatique': {
+        value = 'CULT';
+        break;
+      }
+      default: {
+        //statements; 
+        break;
+      }
+    }
+    this.ok = value + '_';
+    console.log(this.ok);
+    return value;
+  }
+  
+  test(cours) {
+    console.log(cours);
+  }
 
 }
